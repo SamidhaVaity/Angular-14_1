fb85bb8fafd88629fe68009e4e795cef2d7fd115
Samidha Vaity <samidhavaity@gmail.com>
2024-01-15 13:17:06 +0530

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Demo1Component,Demo2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
